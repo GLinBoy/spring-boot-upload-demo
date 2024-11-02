b2e8bafc64f92c3f7e8639aa6ad325855661346e
GLinBoy <glinboy@live.com>
2024-11-02 11:12:25 +0100

    👌 IMPROVE: Add a new endpoint to support multiple file upload

--- src/main/java/com/glinboy/sample/upload/web/controller/UploadController.java
+++ src/main/java/com/glinboy/sample/upload/web/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.glinboy.sample.upload.web.controller;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +22,17 @@ public class UploadController {
     private final FileStorageService fileStorageService;
 
     @PostMapping
-    public String uploadFIle(@RequestParam() MultipartFile file, RedirectAttributes redirectAttributes) {
+    public String uploadFile(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
         Optional<String> stored = fileStorageService.store(file);
         return stored.map(r -> {
             redirectAttributes.addFlashAttribute("message", r);
             return "redirect:/successful.html";
         }).orElseGet(() -> "redirect:/error.html");
+    }
+
+    @PostMapping("/multiple")
+    public String uploadFiles(@RequestParam MultipartFile[] files) {
+        Stream.of(files).forEach(fileStorageService::store);
+        return "redirect:/successful.html";
     }
 }
